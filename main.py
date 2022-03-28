5e3cc3cb5e2e90eb532e0dfb008d7df2f8d8b4bf
A2 <77829156+weizhengchu@users.noreply.github.com>
2022-03-28 15:34:15 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template, request
+from pymysql import connections
+import os
+import boto3
+from config import *
+
+app = Flask(__name__)
+
+bucket = custombucket
+region = customregion
+
+db_conn = connections.Connection(
+    host=customhost,
+    port=3306,
+    user=customuser,
+    password=custompass,
+    db=customdb
+
+)
+output = {}
+table = 'employee'
+
+
+@app.route("/fetchdata", methods=['POST'])
+def fectdata():
+    cursor = db_conn.cursor()
+    cursor.execute("SELECT * FROM employee")
+    employee = cursor.fetchall()
+    return render_template('account.html',employee=employee)
+
+
+if __name__ == '__main__':
+    app.run()
